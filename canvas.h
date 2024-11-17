9e976e63d89b540eec367003836cd4f9596f6f37
Smyran <smyranmath@gmail.com>
2024-11-17 15:26:46 -0700

    Added documentation to header file

--- canvas.h
+++ canvas.h
@@ -19,27 +19,41 @@ class Canvas : public QGraphicsView
 
 public:
 
+    /// @brief Constructor for the Canvas widget.
+    ///
+    /// @param parent - The parent of this widget.
     explicit Canvas(QWidget *parent = nullptr);
 
 public slots:
 
+    /// @brief Creates a box on the canvas representing
+    ///        a basic object/class.
     void addClass();
 
+    /// @brief Creates a box on the canvas representing
+    ///        an abstract class.
     void addAbstractClass();
 
+    /// @brief Creates a box on the canvas representing
+    ///        an interface.
     void addInterface();
 
+    /// @brief Creates a box on the canvas representing
+    ///        an enum.
     void addEnum();
 
 signals:
 
 private:
 
+    /// @brief Defines the size of boxes when they are created.
     const int DEFAULT_BOX_WIDTH = 150;
     const int DEFAULT_BOX_HEIGHT = 50;
 
+    /// @brief The scene this widget uses to draw on.
     QGraphicsScene* world;
 
+    /// @brief Creates and returns a pointer to a template box on the Canvas.
     QGraphicsRectItem* makeClassTemplate(const QString& defaultText);
 
 };
