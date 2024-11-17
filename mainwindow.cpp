cf37d1cea4fe636f4dee09ef198aeaa02969d0b6
Smyran <smyranmath@gmail.com>
2024-11-17 02:03:37 -0700

    Added connection statements for canvas slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,5 +21,9 @@ MainWindow::~MainWindow()
 
 void MainWindow::setupConnections()
 {
+    // Connect toolbar signals to canvas slots
     connect(ui->toolbar, &Toolbar::addClass, ui->canvas, &Canvas::addClass);
+    connect(ui->toolbar, &Toolbar::addAbstractClass, ui->canvas, &Canvas::addAbstractClass);
+    connect(ui->toolbar, &Toolbar::addInterface, ui->canvas, &Canvas::addInterface);
+    connect(ui->toolbar, &Toolbar::addEnum, ui->canvas, &Canvas::addEnum);
 }
